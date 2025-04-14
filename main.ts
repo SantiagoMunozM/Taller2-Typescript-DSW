19dfc1dea5a42450b4f08d997a3276ebedee8df9
SantiagoMunozM <s.munozm234@uniandes.edu.co>
2025-04-14 00:19:35 -0500

    Entrega Taller 2 - Lamento la tarddanza, no me funcionaba porque no me habia acordado de compilar los typescript :(

--- main.ts
+++ main.ts
@@ -14,10 +14,16 @@ function renderSeriesTable(series: Serie[]):void {
     series.forEach((serie) => {
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<td style = "font-weight: bold;">${serie.codigo}</td>
-                               <td style = "color:#007bff !important;">${serie.nombre}</td>
+                               <td style = "color:#007bff;cursor: pointer;" class="serie-nombre">${serie.nombre}</td>
                                <td>${serie.canal}</td>
                                <td>${serie.temporadas}</td>`;
+
+        trElement.addEventListener("click", () => {
+            showSerieCard(serie);
+        });
         seriesTbody.appendChild(trElement);
+
+        
     })
     let lastRow = document.createElement("tr");
     lastRow.innerHTML = `<td colspan = "2" style = "background-color: white !important;"> Seasons average: ${getAverageSeasons(series)}</td>`;
@@ -30,3 +36,20 @@ function getAverageSeasons(series: Serie[]): number {
     let promedioTemporadas: number = totalTemporadas / series.length;
     return promedioTemporadas;
   }
+
+  function showSerieCard(serie: Serie): void {
+    const cardContainer = document.getElementById("serie-card-container")!;
+    cardContainer.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${serie.linkImagen}" alt="Series image">
+            <div class="card-body">
+                <h5 class="card-title">${serie.nombre}</h5>
+                <p class="card-text">${serie.descripcion}</p>
+                <p class="card-text"><strong>Channel:</strong> ${serie.canal}</p>
+                <p class="card-text"><strong>Seasons:</strong> ${serie.temporadas}</p>
+                <a href="${serie.linkSitio}" class="btn btn-link" target="_blank">More info</a>
+            </div>
+        </div>
+    `;
+    cardContainer.style.display = 'block';
+}
--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,10 @@ function renderSeriesTable(series) {
     console.log("Desplegando series");
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td style = \"font-weight: bold;\">".concat(serie.codigo, "</td>\n                               <td style = \"color:#007bff !important;\">").concat(serie.nombre, "</td>\n                               <td>").concat(serie.canal, "</td>\n                               <td>").concat(serie.temporadas, "</td>");
+        trElement.innerHTML = "<td style = \"font-weight: bold;\">".concat(serie.codigo, "</td>\n                               <td style = \"color:#007bff;cursor: pointer;\" class=\"serie-nombre\">").concat(serie.nombre, "</td>\n                               <td>").concat(serie.canal, "</td>\n                               <td>").concat(serie.temporadas, "</td>");
+        trElement.addEventListener("click", function () {
+            showSerieCard(serie);
+        });
         seriesTbody.appendChild(trElement);
     });
     var lastRow = document.createElement("tr");
@@ -17,4 +20,9 @@ function getAverageSeasons(series) {
     series.forEach(function (serie) { return totalTemporadas = totalTemporadas + serie.temporadas; });
     var promedioTemporadas = totalTemporadas / series.length;
     return promedioTemporadas;
+}
+function showSerieCard(serie) {
+    var cardContainer = document.getElementById("serie-card-container");
+    cardContainer.innerHTML = "\n        <div class=\"card\" style=\"width: 18rem;\">\n            <img class=\"card-img-top\" src=\"".concat(serie.linkImagen, "\" alt=\"Series image\">\n            <div class=\"card-body\">\n                <h5 class=\"card-title\">").concat(serie.nombre, "</h5>\n                <p class=\"card-text\">").concat(serie.descripcion, "</p>\n                <p class=\"card-text\"><strong>Channel:</strong> ").concat(serie.canal, "</p>\n                <p class=\"card-text\"><strong>Seasons:</strong> ").concat(serie.temporadas, "</p>\n                <a href=\"").concat(serie.linkSitio, "\" class=\"btn btn-link\" target=\"_blank\">More info</a>\n            </div>\n        </div>\n    ");
+    cardContainer.style.display = 'block';
 }
